3bf14a4aa763e449bd9a1ccdaffe96958a85ffd9
Horanituze <horanituzej@gmail.com>
2022-12-20 17:02:58 -0600

    update eda, model and create FlaskAPI

--- Salaries_model.py
+++ Salaries_model.py
@@ -3,19 +3,20 @@
 """
 Created on Fri Nov 18 12:15:21 2022
 
-@author: jhoranituze2
+@author: Jocelyne Horanituze
 """
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 df = pd.read_csv('eda.csv')
 
 #choose relevant columns
 df.columns
-#let's keep all columns for now
-df_model = df
+
+df_model = df[['work_year', 'experience_level', 'employment_type',
+               'salary_in_usd', 'remote_ratio', 'company_size', 'job_simp',
+               'location_simp','residence_simp']]
 
 # dummy variable for our categorical variables
 df_dum = pd.get_dummies(df_model)
@@ -25,6 +26,7 @@ X = df_dum.drop(['salary_in_usd'], axis=1)
 y = df_dum['salary_in_usd']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 #multiple linear regression
 #from scipy import stats
 import statsmodels.api as sm
@@ -37,56 +39,23 @@ model.fit().summary()
 from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.model_selection import cross_val_score
 
+#lm = LinearRegression(normalize = True)
 lm = LinearRegression()
 lm.fit(X_train,y_train)
 np.mean(cross_val_score(lm,X_train,y_train, scoring = 'neg_mean_absolute_error',cv =3))
 
-'''
-y_pred = lm.predict(X_test)
-mean_absolute_error(y_test, y_pred)
+from sklearn.linear_model import Ridge
+rr = Ridge(alpha=0.01)
+rr.fit(X_train, y_train) 
+np.mean(cross_val_score(rr,X_train,y_train, scoring = 'neg_mean_absolute_error',cv =3))
+rr.score(X_train, y_train)
 
-score = np.mean(cross_val_score(lm,X_train,y_train, scoring = 'neg_mean_absolute_error',cv =3))
-score
-print("%0.2f accuracy with a standard deviation of %0.2f" % (score.mean(), score.std()))
-
-'''
 #LASSO regression 
-lasso = Lasso(alpha = 0.99)
+lasso = Lasso()
 lasso.fit(X_train, y_train)
 np.mean(cross_val_score(lasso,X_train,y_train, scoring = 'neg_mean_absolute_error',cv =3))
+lasso.score(X_train, y_train)
 
-alpha = []
-error = []
-
-for i in range(1,100):
-    alpha.append(i/100)
-    lm_l = Lasso(alpha= i/100)
-    error.append(np.mean(cross_val_score(lm_l,X_train,y_train, scoring = 'neg_mean_absolute_error',cv =3)))
-plt.plot(alpha, error)
-
-err = tuple(zip(alpha,error))
-df_err = pd.DataFrame(err, columns= ['alpha', 'error'])
-df_err[df_err.error == max(df_err.error)]
-
-'''
-#LASSO regression (feature selection)
-from sklearn.linear_model import LassoCV
-lasso = LassoCV()
-lasso.fit(X_train,y_train)
-print('Best alpha using built-in LassoCV: %f' %lasso.alpha_)
-print('Best score using built-in LassoCV: %f' %lasso.score(X,y))
-
-coef = pd.Series(lasso.coef_, index = X.columns)
-print('Lasso picked '+str(sum(coef!=0)) + ' variables and eliminated the other '+str(sum(coef==0))+' variables')
-
-imp_coef = coef.sort_values()
-plt.rcParams['figure.figsize'] = (8, 10)
-imp_coef.plot(kind = 'barh')
-#plt.title("Feature importance using Lasso Model")
-# the selected features
-kept_feat = [feature for feature, weight in zip(X.columns.values, lasso.coef_) if weight != 0]
-kept_feat
-'''
 #Random forest
 from sklearn.ensemble import RandomForestRegressor
 rf = RandomForestRegressor()
@@ -100,7 +69,7 @@ gs.fit(X_train, y_train)
 
 gs.best_score_
 gs.best_estimator_
-#RandomForestRegressor(criterion='mae', max_features='sqrt', n_estimators=80)
+#RandomForestRegressor(criterion='mae', max_features='log2', n_estimators=160)
 #test ensembles
 tpred_lm = lm.predict(X_test)
 tpred_lasso = lasso.predict(X_test)
@@ -113,3 +82,18 @@ mean_absolute_error(y_test, tpred_rf)
 
 #combine the 2 best models
 mean_absolute_error(y_test, (tpred_lasso+tpred_rf)/2)
+
+#Productionize the model in FLASK
+
+import pickle
+pickl = {'model': gs.best_estimator_}
+pickle.dump( pickl, open( 'model_file' + ".p", "wb" ) )
+
+
+file_name = "model_file.p"
+with open(file_name, 'rb') as pickled:
+    data = pickle.load(pickled)
+    model = data['model']
+model.predict(X_test.iloc[1,:].values.reshape(1, -1))
+
+list(X_test.iloc[1,:])
