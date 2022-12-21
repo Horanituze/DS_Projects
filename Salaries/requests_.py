d744d4ced6bbbed702e6eab3676e4440c7fcafc7
Horanituze <44282495+Horanituze@users.noreply.github.com>
2022-12-20 18:19:06 -0600

    add FlaskAPI files

---
+++ Salaries/requests_.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 20 15:18:02 2022
+
+@author: jhoranituze2
+"""
+
+import requests
+from data_input import data_in
+
+URL = 'http://127.0.0.1:5000/predict'
+headers = {"Content-Type": "application/json"}
+data = {"input": data_in}
+
+r = requests.get(URL, headers=headers, json=data)
+
+r.json()
